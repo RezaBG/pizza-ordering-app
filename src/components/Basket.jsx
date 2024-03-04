c4013bb0789e11add119c9ed0ba07bb7ff981a2e
rezaBG <rezabarzegargashti@gmail.com>
2024-03-04 14:02:57 +0100

    remove btn in Basket

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -1,16 +1,26 @@
 import React from "react";
+import "./Basket.css";
 
-const Basket = ({ basket }) => {
+const Basket = ({ basket, removeFromBasket }) => {
+  const totalPrice = basket.reduce((total, item) => total + item.price, 0);
+
   return (
-    <div>
-      <h2>Basket</h2>
-      <ul>
+    <div className="basket-container">
+      <h2 className="basket-title">Basket</h2>
+      <ul className="basket-list">
         {basket.map((item, index) => (
-          <li key={index}>
+          <li key={index} className="basket-item">
             {item.name} - ${item.price}
+            <button
+              className="remove-button"
+              onClick={() => removeFromBasket(item)}
+            >
+              Remove
+            </button>
           </li>
         ))}
       </ul>
+      <p className="total-price">Total: ${totalPrice.toFixed(2)}</p>
     </div>
   );
 };
--- src/components/PizzaList.jsx
+++ src/components/PizzaList.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import pizzas from "../data/pizzas";
+import Basket from "./Basket";
 import "./PizzaList.css";
 
 const PizzaList = () => {
+  const [basket, setBasket] = useState([]);
+
+  const addToBasket = (pizza) => {
+    setBasket([...basket, pizza]);
+  };
+
+  const removeFromBasket = (pizzaToRemove) => {
+    const newBasket = basket.filter((pizza) => pizza.id !== pizzaToRemove.id);
+    setBasket(newBasket);
+  };
+
   return (
     <div>
       <h1>Pizza Menu</h1>
@@ -14,10 +26,12 @@ const PizzaList = () => {
               <h2 className="pizza-name">{pizza.name}</h2>
               <p className="pizza-description">{pizza.description}</p>
               <p className="pizza-price">${pizza.price}</p>
+              <button onClick={() => addToBasket(pizza)}>Add to Basket</button>
             </div>
           </li>
         ))}
       </ul>
+      <Basket basket={basket} removeFromBasket={removeFromBasket} />
     </div>
   );
 };

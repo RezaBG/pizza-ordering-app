51a80a25d1c07da0f062692b30ffd12110e5d520
rezaBG <rezabarzegargashti@gmail.com>
2024-03-04 12:13:29 +0100

    data/pizzas

---
+++ src/data/pizzas.js
@@ -0,0 +1,91 @@
+const pizzas = [
+  {
+    id: 1,
+    name: "Pepperoni Pizza",
+    description:
+      "Classic pepperoni pizza with tomato sauce and mozzarella cheese.",
+    price: 9.99,
+    image: "https://i.ebayimg.com/images/g/lsEAAOSwNJtlF9pE/s-l1600.png",
+  },
+  {
+    id: 2,
+    name: "Margherita Pizza",
+    description:
+      "Traditional pizza topped with tomato sauce, fresh mozzarella, and basil.",
+    price: 8.99,
+    image: "https://milanoweimar.de/wp-content/uploads/pizza-magherita.png",
+  },
+  {
+    id: 3,
+    name: "Vegetarian Pizza",
+    description:
+      "Loaded with a variety of fresh vegetables like bell peppers, onions, and olives.",
+    price: 10.99,
+    image:
+      "https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA5L3Jhd3BpeGVsb2ZmaWNlMTBfcmVhbF9waG90b19vZl92ZWdldGFyaWFuX3BpenphX3RvcF92aWV3X2lzb2xhdF84ZjI1YzljYy00NmZhLTRhYWUtODRlMy02NmYxMmNiYjcyMjlfMS5qcGc.jpg",
+  },
+  {
+    id: 4,
+    name: "Hawaiian Pizza",
+    description:
+      "Sweet and savory pizza with pineapple, ham, and tomato sauce.",
+    price: 11.99,
+    image:
+      "https://admin.screaminsicilian.com/wp-content/uploads/2021/04/product-overhead_SS_Hawaiian.png",
+  },
+  {
+    id: 5,
+    name: "BBQ Chicken Pizza",
+    description:
+      "Tangy BBQ sauce, grilled chicken, red onions, and cilantro on a pizza.",
+    price: 12.99,
+    image:
+      "https://static.vecteezy.com/system/resources/previews/024/108/111/original/tasty-bbq-chicken-cheese-pizza-isolated-on-transparent-background-png.png",
+  },
+  {
+    id: 6,
+    name: "Supreme Pizza",
+    description:
+      "A meat-lover’s dream with pepperoni, sausage, mushrooms, and olives.",
+    price: 13.99,
+    image:
+      "https://png.pngtree.com/png-clipart/20230423/original/pngtree-supreme-pizza-lifted-slice-png-image_9080664.png",
+  },
+  {
+    id: 7,
+    name: "Buffalo Chicken Pizza",
+    description:
+      "Spicy buffalo sauce, grilled chicken, red onions, and ranch drizzle.",
+    price: 12.49,
+    image:
+      "https://png.pngtree.com/png-clipart/20231017/original/pngtree-cheese-and-chicken-pizza-png-image_13327672.png",
+  },
+  {
+    id: 8,
+    name: "Mushroom Pizza",
+    description:
+      "Sliced mushrooms, garlic, ricotta cheese, and mozzarella on a pizza.",
+    price: 10.49,
+    image:
+      "https://thumbs.dreamstime.com/b/top-view-mushroom-pizza-top-view-mushroom-pizza-white-background-297044989.jpg",
+  },
+  {
+    id: 9,
+    name: "Four Cheese Pizza",
+    description:
+      "Blend of mozzarella, parmesan, provolone, and gorgonzola cheeses.",
+    price: 11.49,
+    image: "https://clipground.com/images/pizza-cheese-png-3.png",
+  },
+  {
+    id: 10,
+    name: "Meatball Pizza",
+    description:
+      "Homemade meatballs, marinara sauce, mozzarella, and fresh basil.",
+    price: 12.99,
+    image:
+      "https://img.freepik.com/premium-photo/isolated-fresh-baked-meatball-pizza-with-red-onion_219193-8172.jpg?w=360",
+  },
+];
+
+export default pizzas;

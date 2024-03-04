b7fae28fed2089f367006207f24231591006db34
rezaBG <rezabarzegargashti@gmail.com>
2024-03-04 14:56:14 +0100

    add-basket-quantity

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Basket.css";
 
 const Basket = ({ basket, removeFromBasket }) => {
-  const totalPrice = basket.reduce((total, item) => total + item.price, 0);
+  const [items, setItems] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    const uniqueItems = basket.reduce((acc, item) => {
+      const existingItem = acc.find((i) => i.id === item.id);
+      if (existingItem) {
+        existingItem.quantity += 1;
+        existingItem.totalPrice += item.price;
+      } else {
+        acc.push({
+          id: item.id,
+          name: item.name,
+          quantity: 1,
+          totalPrice: item.price,
+        });
+      }
+      return acc;
+    }, []);
+    setItems(uniqueItems);
+
+    // Calculate total price of all items in the basket
+    const newTotalPrice = basket.reduce((total, item) => total + item.price, 0);
+    setTotalPrice(newTotalPrice);
+  }, [basket]);
 
   return (
     <div className="basket-container">
       <h2 className="basket-title">Basket</h2>
       <ul className="basket-list">
-        {basket.map((item, index) => (
+        {items.map((item, index) => (
           <li key={index} className="basket-item">
-            {item.name} - ${item.price}
+            <span>
+              {item.name}: {item.quantity}x - ${item.totalPrice.toFixed(2)}
+            </span>
             <button
               className="remove-button"
               onClick={() => removeFromBasket(item)}
